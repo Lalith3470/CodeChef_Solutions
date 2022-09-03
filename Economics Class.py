17765e2bca7426f31436f5527efe90dcf071b0ed
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-09-03 13:52:11 +0530

    Create Economics Class.py

---
+++ Economics Class.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    
+    count = 0
+    for k in range(n):
+        s = input()
+    
+    for j in range(n):
+        d = input()
+        
+    a = s.split()
+    b = d.split()
+        
+    for l in range(n):
+        if a[l] == b[l]:
+            count += 1
+    print(count)
