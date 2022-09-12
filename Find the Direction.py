c5dd5743b446fa02c9e040ace55ce342ea6ff9c0
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-09-12 19:05:40 +0530

    Create Find the Direction.py

---
+++ Find the Direction.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    X = int(input())
+    if X%4 == 1:
+        print("East")
+    elif X%4 == 2:
+        print("South")
+    elif X%4 == 3:
+        print("West")
+    else:
+        print("North")
