c175cee88a8598f8c54254dae74f8a354bf31a41
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-09-13 18:36:47 +0530

    Create Finding Square Roots.c

---
+++ Finding Square Roots.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+	int t,n,r;
+	scanf("%d",&t);
+	while(t--)
+	{
+	scanf("%d",&n);  
+	r=sqrt(n);
+	printf("%d\n",r);
+	      
+	      
+	}
+	return 0;
+}
+
