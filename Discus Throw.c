e8ffe0415b147bface9a9d41ff389990c2e013b4
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-09-13 18:35:49 +0530

    Create Discus Throw.c

---
+++ Discus Throw.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	while(n--){
+	    int a,b,c;
+	    scanf("%d %d %d",&a,&b,&c);
+	    if(a >= b){
+	        if(a >= c)
+	            printf("%d\n",a);
+	        else  printf("%d\n",c);     
+	    }
+	    else{
+	        if(b >= c)
+	            printf("%d\n",b);
+	        else printf("%d\n",c);
+	    }
+	}
+	return 0;
+}
