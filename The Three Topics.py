32fe8c5a18dd37d455901027958ee38c8135f499
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-09-16 19:01:07 +0530

    Create The Three Topics.py

---
+++ The Three Topics.py
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b,c,x;
+	scanf("%d %d %d %d",&a,&b,&c,&x);
+	if(x <= 3)
+	    printf("Yes");
+	else printf("No");
+	return 0;
+}
+
