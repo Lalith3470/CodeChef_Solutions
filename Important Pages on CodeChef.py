4521021314129d2443e6e781eeeeca35bf96497c
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2022-09-18 20:20:43 +0530

    Create Important Pages on CodeChef.py

---
+++ Important Pages on CodeChef.py
@@ -0,0 +1,10 @@
+A,B=map(int,input().split())
+
+if A==0 and (B==1 or B==0):
+    print("https://www.codechef.com/practice")
+
+elif A==1 and B==0:
+    print("https://www.codechef.com/contests")
+
+elif A==1 and B==1:
+    print("https://discuss.codechef.com")
