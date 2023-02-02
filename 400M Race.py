bec93d287cce6443a049ed194a19e92ecffe301d
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-02-02 21:51:09 +0530

    Create 400M Race.py

---
+++ 400M Race.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    lst=list(map(int, input().split()))
+    lst[0]=400/lst[0]
+    lst[1]=400/lst[1]
+    lst[2]=400/lst[2]
+    if max(lst)==lst[0]:
+        print("ALICE")
+    elif max(lst)==lst[1]:
+        print("BOB")
+    else:
+        print("CHARLIE")
